a9bfa5bdfc478402cdd0ed7951009b9f7bbc4581
Ravi Utsav <raviutsav0@gmail.com>
2024-01-26 18:24:37 +0530

    done as of now

--- api.py
+++ api.py
@@ -1,97 +1,84 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify, make_response
 import pandas as pd
 import subprocess, re
-
+from helper_functions import read_and_store_data, query_data, empty_file
 app = Flask(__name__)
 
-# central_data_repository will map 'link' to its corresponding dataframe
-central_data_repository = {}
+interaction_data = {}
 urlFilePath = 'url.txt'
 outputFilePath = 'output.txt'
-
-# function to clear the content of file
-def empty_file(file_path):
-    open(file_path, 'w').close()
-
-# function to store the data in their corresponding dataframe
-def read_and_store_data(file_path):
-    with open(file_path, 'r') as file:
-        for line in file:
-            line = line.replace('\x00','')
-            data_in_list = line.split(' ')
-
-            link = data_in_list[0][1:-1]
-            datestamp = data_in_list[-2]
-            timestamp = data_in_list[-1]
-
-            new_data = {'datetime': pd.to_datetime(datestamp + " " + timestamp), 'value': ' '.join(data_in_list[1:])}
-            central_data_repository[link] = central_data_repository[link].append(new_data, ignore_index=True)
-    
-    empty_file(file_path)
-
-def query_data(start_time, end_time, df):
-    mask = (df['datetime'] >= start_time) & (df['datetime'] <= end_time)
-    result = df[mask]
-    return result
 
 @app.route('/')
 def home():
     return 'Hello, World!\n'
 
-
 @app.route('/getURL', methods=['GET'])
 def getURL():
-    empty_file(urlFilePath)
-    read_and_store_data(outputFilePath)
 
-    command = "(./interactsh-client | tee " + outputFilePath + ") 3>&1 1>&2 2>&3 | tee " + urlFilePath
+    try:
+        empty_file(urlFilePath)
+        read_and_store_data(outputFilePath, interaction_data)
 
-    subprocess.Popen(command, shell=True, text=True)
+        command = "(./interactsh-client | tee " + outputFilePath + ") 3>&1 1>&2 2>&3 | tee " + urlFilePath
 
-    content = ''
-    oast_link_regex = re.compile(r'.*oast.*')
+        subprocess.Popen(command, shell=True, text=True)
 
-    while len(content) == 0:
-        with open(urlFilePath, 'r') as file:
-            content = file.read()
-            content = oast_link_regex.findall(content)
-    
-    link = content[0].split()[1]
+        content = ''
+        oast_link_regex = re.compile(r'.*oast.*')
 
-    df = pd.DataFrame(columns=['datetime', 'value'])
-    central_data_repository[link.split('.')[0]] = df
+        while len(content) == 0:
+            with open(urlFilePath, 'r') as file:
+                content = file.read()
+                content = oast_link_regex.findall(content)
+        
+        link = content[0].split()[1]
 
-    return link
+        df = pd.DataFrame(columns=['datetime', 'value'])
+        interaction_data[link.split('.')[0]] = df
+
+        return link
+
+    except Exception as e:
+        print(f"An error occured: {e}")
+        response = make_response(jsonify({'error': 'Internal Server Error'}), 500)
+        return response
     
 
 
 @app.route('/getInteractions', methods=['GET'])
 def getInteractions():
-    read_and_store_data(outputFilePath)
-    
-    link = request.args.get('link').split('.')[0]
 
-    if 'startDateTime' in request.args:
-        start_datetime = pd.to_datetime(request.args.get('startDateTime'))
-    else:
-        start_datetime = pd.to_datetime('1970-01-01 00:00:00')
-
-    if 'endDateTime' in request.args:
-        end_datetime = pd.to_datetime(request.args.get('endDateTime'))
-    else:
-        end_datetime = pd.Timestamp.now()
-    
-    df = central_data_repository[link]
-    
-    df = query_data(start_datetime, end_datetime, df)
-
-    data_in_dict = df.to_dict()
-    data_in_list = []
-
-    for key, value in data_in_dict['value'].items():
-        data_in_list.append(value)
+    try:
+        read_and_store_data(outputFilePath, interaction_data)
         
-    return data_in_list
+        link = request.args.get('link').split('.')[0]
+
+        if 'startDateTime' in request.args:
+            start_datetime = pd.to_datetime(request.args.get('startDateTime'))
+        else:
+            start_datetime = pd.to_datetime('1970-01-01 00:00:00')
+
+        if 'endDateTime' in request.args:
+            end_datetime = pd.to_datetime(request.args.get('endDateTime'))
+        else:
+            end_datetime = pd.Timestamp.now()
+        
+        df = interaction_data[link]
+        
+        df = query_data(start_datetime, end_datetime, df)
+
+        data_in_dict = df.to_dict()
+        data_in_list = []
+
+        for key, value in data_in_dict['value'].items():
+            data_in_list.append(value)
+
+        return data_in_list
+    
+    except Exception as e:
+        print(f"An error occured: {e}")
+        response = make_response(jsonify({'error': 'Internal Server Error'}), 500)
+        return response
 
 
 if __name__ == '__main__':
