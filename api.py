f0364d1132555639b4cbf477a10e03181d36b499
Ravi Utsav <raviutsav0@gmail.com>
2024-01-26 17:49:19 +0530

    make code readable

--- api.py
+++ api.py
@@ -1,30 +1,25 @@
 from flask import Flask, request
 import pandas as pd
-import subprocess
-import time
-import re
+import subprocess, re
 
 app = Flask(__name__)
 
-# Define a basic route
-
-# this hash_map will store key as
-# the url, value as the dataframe
-
-# the dataframe structure will be
-# column-> datetime | value |
-
+# central_data_repository will map 'link' to its corresponding dataframe
 central_data_repository = {}
+urlFilePath = 'url.txt'
+outputFilePath = 'output.txt'
 
+# function to clear the content of file
 def empty_file(file_path):
     open(file_path, 'w').close()
 
+# function to store the data in their corresponding dataframe
 def read_and_store_data(file_path):
     with open(file_path, 'r') as file:
         for line in file:
             line = line.replace('\x00','')
             data_in_list = line.split(' ')
-            print('LINEEE ' + line)
+
             link = data_in_list[0][1:-1]
             datestamp = data_in_list[-2]
             timestamp = data_in_list[-1]
@@ -34,6 +29,10 @@ def read_and_store_data(file_path):
     
     empty_file(file_path)
 
+def query_data(start_time, end_time, df):
+    mask = (df['datetime'] >= start_time) & (df['datetime'] <= end_time)
+    result = df[mask]
+    return result
 
 @app.route('/')
 def home():
@@ -42,17 +41,10 @@ def home():
 
 @app.route('/getURL', methods=['GET'])
 def getURL():
-    empty_file('url.txt')
-    read_and_store_data('output.txt')
-    empty_file('output.txt')
-    # start a new instance of interact.sh
-    # command > out 2>error
-    # ./program 2>&1 | tee a.txt
-    # command = "./interactsh-client > output.txt 2>url.txt"
-    # command = "./interactsh-client 2>&1 | tee url.txt"
-    command = "(./interactsh-client | tee output.txt) 3>&1 1>&2 2>&3 | tee url.txt"
+    empty_file(urlFilePath)
+    read_and_store_data(outputFilePath)
 
-    file_path = 'url.txt'
+    command = "(./interactsh-client | tee " + outputFilePath + ") 3>&1 1>&2 2>&3 | tee " + urlFilePath
 
     subprocess.Popen(command, shell=True, text=True)
 
@@ -60,16 +52,11 @@ def getURL():
     oast_link_regex = re.compile(r'.*oast.*')
 
     while len(content) == 0:
-        with open(file_path, 'r') as file:
+        with open(urlFilePath, 'r') as file:
             content = file.read()
             content = oast_link_regex.findall(content)
     
     link = content[0].split()[1]
-    print('my content is here ' + link)
-    
-    
-    print("here is the link " + link)
-    # Use re.search to check for a match
 
     df = pd.DataFrame(columns=['datetime', 'value'])
     central_data_repository[link.split('.')[0]] = df
@@ -80,40 +67,30 @@ def getURL():
 
 @app.route('/getInteractions', methods=['GET'])
 def getInteractions():
-    read_and_store_data('output.txt')
-    empty_file('output.txt')
+    read_and_store_data(outputFilePath)
+    
     link = request.args.get('link').split('.')[0]
 
-    isStartDateTimePresent = False
-    isEndDateTimePresent = False
-
     if 'startDateTime' in request.args:
-        isStartDateTimePresent = True
-        start_datetime = request.args.get('startDateTime')
+        start_datetime = pd.to_datetime(request.args.get('startDateTime'))
+    else:
+        start_datetime = pd.to_datetime('1970-01-01 00:00:00')
+
     if 'endDateTime' in request.args:
-        isEndDateTimePresent = True
-        end_datetime = request.args.get('endDateTime')
-    
-    if isStartDateTimePresent and isEndDateTimePresent:
-        start_datetime = pd.to_datetime(start_datetime)
-        end_datetime = pd.to_datetime(end_datetime)
+        end_datetime = pd.to_datetime(request.args.get('endDateTime'))
+    else:
+        end_datetime = pd.Timestamp.now()
     
     df = central_data_repository[link]
-    def query_data(start_time, end_time):
-        mask = (df['datetime'] >= start_time) & (df['datetime'] <= end_time)
-        result = df[mask]
-        return result
     
-
-    if isStartDateTimePresent and isEndDateTimePresent:
-        df = query_data(start_datetime, end_datetime)
-
+    df = query_data(start_datetime, end_datetime, df)
 
     data_in_dict = df.to_dict()
     data_in_list = []
 
     for key, value in data_in_dict['value'].items():
         data_in_list.append(value)
+        
     return data_in_list
 
 
